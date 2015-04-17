5128ca9364ec1c3d447f89ba21d6ba3d464e7be9
federicobruzone <federicobruzone@users.noreply.github.com>
2015-04-17 16:32:36 -0300

    Update verificador_paridad.cpp

--- verificador_paridad.cpp
+++ verificador_paridad.cpp
@@ -4,7 +4,7 @@ int main() {
 	
 	using namespace std;
 	int x;
-	int c = 1;
+	int c = 0x1;
 	
 	cout << "Inserte el numero cuya paridad desea determinar: ";
 	cin >> x;
@@ -12,7 +12,7 @@ int main() {
 	// Usamos el operador & (AND binario). Si el ultimo bit de la variable int "x" tiene valor
 	// '1' entonces ese bit es comparado bit a bit con el int "c", todos cuyos bits tienen 
 	// valor binario '0', excepto el ultimo (exponente 2^0, determina la paridad de toda la 
-	// expresion). Si el int "x" es impar, la expresión (x & c) se evalua identica a "c", 
+	// expresion). Si el int "x" es impar, la expresión (x & c) se evalua identica a "c" (0x1), 
 	// que equivale al valor booleano de 'true'. En caso contrario la expresión se evalua
 	// igual a 0x0, que equivale al valor booleano de 'false'.
 	
