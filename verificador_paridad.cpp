b3a58225b144d4e7c2ba40f3a4566b21c40f48a3
federicobruzone <federicobruzone@users.noreply.github.com>
2015-04-22 00:33:03 -0300

    Update verificador_paridad.cpp

--- verificador_paridad.cpp
+++ verificador_paridad.cpp
@@ -1,11 +1,30 @@
+/* UTN-FRBA-AED
+ * TP Nº 2: Paridad de enteros
+ * Alumno: Federico León Bruzone [155-683.6] 
+ * Última actualización: 22/04/2015
+ */ 
+
 #include <iostream>
+
+void imprimBanner()
+{
+	using namespace std;
+
+	cout << "\t+---------------------------------+" << endl;
+	cout << "\t|Algoritmos y Estructuras de Datos|" << endl;
+	cout << "\t|UTN-FRBA-Curso K1091             |" << endl;
+	cout << "\t|TP No 2 - Paridad de enteros     |" << endl;
+	cout << "\t+---------------------------------+\n\n" << endl;
+	
+}
 
 int main() {
 	
 	using namespace std;
 	int x;
-	int c = 0x1;
+	//int c = 0x1;
 	
+	imprimBanner();
 	cout << "Inserte el numero cuya paridad desea determinar: ";
 	cin >> x;
 	
@@ -16,6 +35,9 @@ int main() {
 	// que equivale al valor booleano de 'true'. En caso contrario la expresión se evalua
 	// igual a 0x0, que equivale al valor booleano de 'false'.
 	
-	if (x & c) {cout << "No es par";} 
+	// if (x & c) {cout << "No es par";} // 
+	// else {cout << "Es par";}
+	
+	if (x % 2 == 0) {cout << "No es par";} 
 	else {cout << "Es par";}
 }
